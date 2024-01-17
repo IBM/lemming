f7dc3c9e06e5c3cc2883725746e6ab8fae99b297
Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
2024-01-17 16:21:52 -0400

    forbiditerative update
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>

--- server/planners/drivers/landmark_driver.py
+++ server/planners/drivers/landmark_driver.py
@@ -1,55 +1,37 @@
 from pathlib import Path
-from typing import List, Optional
-import json
-import forbiditerative
+from typing import List
 import tempfile
-import subprocess
-import sys
 
+from forbiditerative import planners
 from planners.drivers.landmark_driver_datatype import Landmark
-
-build_dir = Path(forbiditerative.__file__).parent / "builds" / "release" / "bin"
-
-
-def parse_landmarks(result: Path) -> List[Landmark]:
-    data = json.loads(result.read_text())
-    return [
-        Landmark(
-            facts=landmark["facts"],
-            disjunctive=eval(landmark["disjunctive"]),
-            first_achievers=landmark["first_achievers"],
-        )
-        for landmark in data["landmarks"]
-    ]
+from helpers.planner_helper.planner_helper_data_types import LandmarkCategory
 
 
 def get_landmarks(
     category: str,
     domain: str,
     problem: str,
-    case_sensitive: Optional[bool] = False,
 ) -> List[Landmark]:
-    """Execute the planner on the task, no search."""
-    # a mapping from category to aliases recognized by the planner
-    # each alias represents a large amount of settings.
-    with tempfile.NamedTemporaryFile() as domain_temp, tempfile.NamedTemporaryFile() as problem_temp:
-        # We have to read and write to files because the planner is CLI oriented.
-        landmarks_file = Path(tempfile.gettempdir()) / "landmarks.json"
+    if category is not LandmarkCategory.RWH.value:
+        raise NotImplementedError
+
+    with (
+        tempfile.NamedTemporaryFile() as domain_temp,
+        tempfile.NamedTemporaryFile() as problem_temp,
+    ):
         domain_file = Path(tempfile.gettempdir()) / domain_temp.name
         problem_file = Path(tempfile.gettempdir()) / problem_temp.name
         domain_file.write_text(domain)
         problem_file.write_text(problem)
-        translator_options = (
-            ["--translate-options", "--case-sensitive", "--search-options"]
-            if case_sensitive
-            else []
+
+        result = planners.get_landmarks(
+            domain_file=domain_file, problem_file=problem_file
         )
-        subprocess.run(
-            [sys.executable, "-B", "-m", "driver.main"]
-            + ["--build", str(build_dir.absolute())]
-            + ["--alias", f"get_landmarks_{category}"]
-            + [str(domain_file.absolute()), str(problem_file.absolute())]
-            + translator_options,
-            cwd=Path(tempfile.gettempdir()),
-        )
-        return parse_landmarks(landmarks_file)
+        return [
+            Landmark(
+                facts=landmark["facts"],
+                disjunctive=landmark["disjunctive"] == "True",
+                first_achievers=landmark["first_achievers"],
+            )
+            for landmark in result["landmarks"]
+        ]

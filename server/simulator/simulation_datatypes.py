d8e64e4e7550c359cf144b0d127ea6ca9463fe03
Jung koo Kang <jungkoo.kang@gmail.com>
2024-01-22 14:29:22 -0500

    Create datatypes for simulator

---
+++ server/simulator/simulation_datatypes.py
@@ -0,0 +1,29 @@
+from typing import Dict, List, Optional
+from pydantic import BaseModel
+from planners.drivers.landmark_driver_datatype import Landmark
+
+
+class SimulationResultUnit(BaseModel):
+    chosen_edge: Optional[str]
+    is_edge_selected: Optional[bool]
+    num_remaining_plans: int
+    is_from_landmark: Optional[bool]
+    is_disambiguation_done: bool
+
+
+class EdgeSelectionPayload(BaseModel):
+    selected_edge: Optional[str]
+    is_edge_selected: bool
+    is_edge_from_landmark: bool
+    plan_hashes: Optional[List[str]]
+
+
+class EdgeSelectionUnit(BaseModel):
+    edge: Optional[str]
+    plan_hashes: Optional[List[str]]
+    landmark: Optional[Landmark]
+
+
+class EdgeChoiceUnit(BaseModel):
+    edge_name_plan_hash_dict: Dict[str, List[str]]
+    landmark: Optional[Landmark]

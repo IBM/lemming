7e516b90d5bac2ff12f075a5a89877e3b3888fae
Jung koo Kang <jungkoo.kang@gmail.com>
2024-01-24 12:48:00 -0500

    Create classes for recording results from simulations

--- server/helpers/common_helper/file_helper.py
+++ server/helpers/common_helper/file_helper.py
@@ -1,13 +1,49 @@
 from __future__ import annotations
-
+import json
+from fastapi import File, UploadFile
+from os import listdir
+from os.path import join, abspath, isfile, isdir
+from pathlib import Path
+from typing import List
+from pydantic import BaseModel
 from typing import TYPE_CHECKING, Any, Optional, Union
 
 if TYPE_CHECKING:
     from _typeshed import SupportsRead
 
-import json
 
-from fastapi import File, UploadFile
+def create_folders_if_not_exist(path_str: str):
+    path = Path(path_str)
+    if not path.exists():
+        path.mkdir(parents=True)
+
+
+def create_file(file_path: Path, str_to_write: str = "") -> None:
+    with open(file_path, "w") as f:
+        f.write(str_to_write)
+
+
+def create_file_from_base_model(file_path: Path, model: BaseModel) -> None:
+    create_file(file_path=file_path, str_to_write=model.model_dump_json())
+
+
+def get_model_from_file(file_path: Path, model: BaseModel) -> BaseModel:
+    with open(file_path, "r") as f:
+        tmp_dict = json.load(f)
+    return model.model_validate(tmp_dict)
+
+
+def append_string_to_file(file_path: Path, str_to_add: str) -> None:
+    with open(file_path, "a") as f:
+        f.write(str_to_add)
+
+
+def get_subfolder_paths_in_folder(root_path: str) -> List[str]:
+    return [abspath(join(root_path, name)) for name in listdir(root_path) if isdir(join(root_path, name))]
+
+
+def get_file_paths_in_folder(root_path: str) -> List[str]:
+    return [abspath(join(root_path, name)) for name in listdir(root_path) if isfile(join(root_path, name))]
 
 
 def read_str_from_upload_file(file: UploadFile = File(...)) -> Optional[str]:
--- server/simulator/simulation_datatypes.py
+++ server/simulator/simulation_datatypes.py
@@ -1,6 +1,48 @@
 from typing import Dict, List, Optional
 from pydantic import BaseModel
 from planners.drivers.landmark_driver_datatype import Landmark
+from helpers.planner_helper.planner_helper_data_types import (
+    Landmark,
+    PlanDisambiguationView,
+    PlanningTask,
+    LandmarkCategory,
+)
+from helpers.common_helper.file_helper import read_str_from_file
+
+
+class PlannerInput(BaseModel):
+    domain: str = ""
+    problem: str = ""
+    num_plans: int
+    quality_bound: float
+    timeout: Optional[int]
+    case_sensitive: bool
+    action_name_prefix_preserve: Optional[str]
+
+    def set_domain_problem(self, domain_file_path: str, problem_file_path: str) -> None:
+        self.domain = read_str_from_file(domain_file_path)
+        self.problem = read_str_from_file(problem_file_path)
+
+    def get_planning_task(self) -> PlanningTask:
+        return PlanningTask(
+            domain=self.domain,
+            problem=self.problem,
+            num_plans=self.num_plans,
+            quality_bound=self.quality_bound,
+            timeout=self.timeout,
+            case_sensitive=self.case_sensitive,
+            action_name_prefix_preserve=self.action_name_prefix_preserve
+        )
+
+
+class SimulationInput(BaseModel):
+    plan_disambiguator_view: PlanDisambiguationView
+    landmark_category: LandmarkCategory
+    select_edge_randomly: bool
+    use_landmark_to_select_edge: bool
+    num_replicates: int
+    setting_name: str
+    planner_input: PlannerInput
 
 
 class SimulationResultUnit(BaseModel):
@@ -9,6 +51,12 @@ class SimulationResultUnit(BaseModel):
     num_remaining_plans: int
     is_from_landmark: Optional[bool]
     is_disambiguation_done: bool
+    landmark_category: LandmarkCategory
+
+
+class SimulationOutput(BaseModel):
+    simulation_results: List[List[SimulationResultUnit]]
+    simulation_input: SimulationInput
 
 
 class EdgeSelectionPayload(BaseModel):

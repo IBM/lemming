1c03eedeb44b083a56110c5064f50a380d1a1e84
Jung koo Kang <jungkoo.kang@gmail.com>
2024-02-01 11:11:08 -0500

    Creating class to record metrics

--- server/simulator/simulation_datatypes.py
+++ server/simulator/simulation_datatypes.py
@@ -20,6 +20,12 @@ class SimulationInput(BaseModel):
     planning_task: PlanningTask
 
 
+class SimulationMetrics(BaseModel):
+    num_edges_chosen: int
+    num_landmarks_chosen: int
+    is_disambiguation_done: bool
+
+
 class SimulationResultUnit(BaseModel):
     chosen_edge: Optional[str]
     is_edge_selected: Optional[bool]
@@ -32,6 +38,18 @@ class SimulationOutput(BaseModel):
     simulation_results: List[List[SimulationResultUnit]]
     simulation_input: SimulationInput
 
+    def get_num_landmarks_from_chosen_edge(self, List[SimulationResultUnit]) -> int:
+        return len(filter lambda edge: edge., self.chosen_edge)
+
+    def get_simulation_metrics(self) -> List[SimulationMetrics]:
+        return list(
+            map(lambda simulation_result: SimulationMetrics(
+            num_edges_chosen=(len(simulation_result.chosen_edge) -1 if simulation_result.chosen_edge is not None and len(simulation_result.chosen_edge) > 1 else 0),
+            num_landmarks_chosen=0,
+            is_disambiguation_done=False
+        ), self.simulation_results)
+        )
+        
 
 class EdgeSelectionPayload(BaseModel):
     selected_edge: Optional[str]
